96f121bf3d1b0a9ec10e4511bff7b265a5667014
Wilhelm Söderkvist Vermelin <wilhelm.wermelin@icloud.com>
2023-12-21 12:46:45 +0100

    problem was duplicate rows..., not fixing now

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 pub mod extract;
 pub mod feature_extractors;
-pub mod load;
 
 use crate::extract::{lazy_feature_df, DynamicGroupBySettings, ExtractionSettings, FeatureSetting};
 use pyo3::prelude::*;
