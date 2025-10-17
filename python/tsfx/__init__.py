dd9f47e6c54d8eb063a75868dbe1194de8db78fb
Wilhelm Söderkvist Vermelin <wilhelm.wermelin@icloud.com>
2025-10-17 09:43:22 +0200

    make typecheckers happy

--- python/tsfx/__init__.py
+++ python/tsfx/__init__.py
@@ -1,2 +1,10 @@
-from tsfx._tsfx import extract_features
-from tsfx.tsfx import DynamicGroupBySettings, ExtractionSettings, FeatureSetting
+from ._tsfx import extract_features
+from .tsfx import DynamicGroupBySettings, ExtractionSettings, FeatureSetting
+
+# Re-export public API for runtime consumers and type checkers (PEP 561).
+__all__ = [
+    "extract_features",
+    "DynamicGroupBySettings",
+    "ExtractionSettings",
+    "FeatureSetting",
+]
